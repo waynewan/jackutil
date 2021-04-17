e512ad16c04436267c940518af929778fb77c01f
wayne.wan.peek <12496043+waynewan@users.noreply.github.com>
2021-04-16 22:44:00 -0400

    bug fix

--- unittest/test_jackutil.py
+++ unittest/test_jackutil.py
@@ -1,11 +1,12 @@
 # --
 # --
 # --
-import jackutil.configuration as cfg
+from pprint import pprint
+from jackutil.configuration import configuration
 
 basespec = { "a": { "x": 1, "y": 99 }, "pp":456 }
 var = { "a/x": [ 23, 34] , ("a/y","pp"):[ (88,12), (77,33) ]}
-cfg = cfg.configuration(basespec=basespec,variations=var)
+cfg = configuration(basespec=basespec,variations=var)
 vv = cfg.all_configurations()
 print(vv)
 print(len(vv))
@@ -45,9 +46,55 @@ print(type(dt_dt), dt_dt)
 # --
 # --
 from jackutil import auditedvalue
-import pprint
 v1 = auditedvalue.AuditedValue()
 v1.setvalue(33,date=datetime.utcnow(),msg="first value")
 v1.setvalue(99,date=datetime.utcnow(),msg="second value")
-pprint.pprint(v1.audit)
-pprint.pprint(v1)
+pprint(v1.audit)
+pprint(v1)
+
+# --
+# -- simple delta for configuraiton
+# --
+basespec = {
+	"a" : 1,
+	"b" : 2,
+	"x" : { "c" : 3, "d" : 4 }
+}
+delta = { "a" : range(3, 20, 3) }
+cc = configuration(basespec=basespec,variations=delta)
+print("=" * 30)
+for c in cc.all_configurations():
+	pprint(c)
+
+# --
+# -- simple delta 2 for configuraiton
+# --
+basespec = {
+	"a" : 1,
+	"b" : 2,
+	"x" : { "c" : 3, "d" : 4 }
+}
+delta = { 
+	"a" : range(3, 20, 3),
+	"x/c" : range(4, 20, 4) 
+}
+cc = configuration(basespec=basespec,variations=delta)
+print("=" * 30)
+for c in cc.all_configurations():
+	pprint(c)
+
+# --
+# -- vector delta
+# --
+delta = { 
+	("x/d","x/c") : [
+		(d,c)
+		for d in range(3, 20, 3)
+	 	for c in range(4, 20, 4) 
+		if(d>c)
+	]
+}
+cc = configuration(basespec=basespec,variations=delta)
+print("=" * 30)
+for c in cc.all_configurations():
+	pprint(c)
