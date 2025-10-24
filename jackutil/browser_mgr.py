8972c3ffcff105ef79ed27fe1f145e98731234a4
wayne@emp-003 <wayne@emp-003>
2025-10-24 00:44:12 -0400

    BUGFIX: quick fix to create browser instance in Windows env (less options)

--- jackutil/browser_mgr.py
+++ jackutil/browser_mgr.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 
+import platform
 import argparse
 import datetime
 import getpass
@@ -24,6 +25,15 @@ CHROME_BINARY_LOCATION='/usr/bin/google-chrome'
 CHROME_WEBDRIVER_BINARY_LOCATION='/usr/bin/chromedriver'
 
 def create_new_browser(rootdir=None,persist_name=None,incognito=True,driver_bin_loc=CHROME_WEBDRIVER_BINARY_LOCATION,browser_bin_loc=CHROME_BINARY_LOCATION):
+	params = locals().copy()
+	un = platform.uname()
+	if(un.system=="Linux"):
+		return __create_new_browser_linux(**params)
+	elif(un.system=="Windows"):
+		return __create_new_browser_win11(**params)
+	raise ValueError(f"Do not know what to do with system type: {un.system}")
+
+def __create_new_browser_linux(rootdir=None,persist_name=None,incognito=True,driver_bin_loc=CHROME_WEBDRIVER_BINARY_LOCATION,browser_bin_loc=CHROME_BINARY_LOCATION):
 	if(rootdir is None):
 		rootdir = tempfile.TemporaryDirectory()
 	options = webdriver.ChromeOptions()
@@ -61,6 +71,38 @@ def create_new_browser(rootdir=None,persist_name=None,incognito=True,driver_bin_
 	driver = webdriver.Chrome(options=options,executable_path=driver_bin_loc)
 	if(persist_name is not None):
 		persist_connection_info(driver=driver,persist_name=persist_name)
+	return driver
+
+def __create_new_browser_win11(rootdir=None,persist_name=None,incognito=True,driver_bin_loc=CHROME_WEBDRIVER_BINARY_LOCATION,browser_bin_loc=CHROME_BINARY_LOCATION):
+	if(rootdir is None):
+		rootdir = tempfile.TemporaryDirectory()
+	options = webdriver.ChromeOptions()
+	options.add_argument("--start-maximized")
+	# -- ---------------------------------------------------------------------
+	# !! try to avoid being detected as automated session
+	# -- ---------------------------------------------------------------------
+	# -- ---------------------------------------------------------------------
+	# !! based on idea from (doesn't work)
+	# !! https://stackoverflow.com/questions/33225947/can-a-website-detect-when-you-are-using-selenium-with-chromedriver
+	# !!
+	# options.add_argument("user-agent=Chrome")
+	# -- ---------------------------------------------------------------------
+	# -- ---------------------------------------------------------------------
+	# !! based on idea from (working)
+	# !! https://piprogramming.org/articles/How-to-make-Selenium-undetectable-and-stealth--7-Ways-to-hide-your-Bot-Automation-from-Detection-0000000017.html
+	# !! TDA: working as of 2022-03-17
+	# !! fidelity: working as of 2022-03-17
+	options.add_experimental_option("excludeSwitches", ["enable-automation"])
+	options.add_experimental_option('useAutomationExtension', False)
+	options.add_argument('--disable-blink-features=AutomationControlled')
+	pprint(options)
+	# -- ---------------------------------------------------------------------
+	# --
+	# --
+	# --
+	if(incognito):
+		options.add_argument("--incognito")
+	driver = webdriver.Chrome(options=options)
 	return driver
 
 def persist_connection_info(*,driver=None,sessionURL=None,sessionID=None,persist_name):
